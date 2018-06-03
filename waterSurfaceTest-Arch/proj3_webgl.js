591121ed84956e3e9e4907563a5d28e8f927eb9a
Archana <archana.sk19@gmail.com>
2018-06-03 13:06:44 -0700

    rendered skybox and mesh together

--- waterSurfaceTest-Arch/proj3_webgl.js
+++ waterSurfaceTest-Arch/proj3_webgl.js
@@ -16,7 +16,7 @@ function initGL(canvas) {
 
 
 /*
- * Initializing shaders 
+ * Initializing cloth mesh shaders 
  */
 var shaderProgram;
 function createShader(vs_id, fs_id) {
@@ -40,10 +40,6 @@ function initShaders() {
     gl.useProgram(shaderProgram);    
 }
 
-function initShaders2() {
-    shaderProgram = createShader("tex-shader-vs", "tex-shader-fs");
-    gl.useProgram(shaderProgram);    
-}
 
 var mvMatrixStack = [];
 /**
@@ -97,24 +93,235 @@ function updateBuffers() {
     gl.bufferData(gl.ARRAY_BUFFER, Float32Array.from(vertexNormal), gl.DYNAMIC_DRAW);
 }
 
-//arch-skybox
 
 //function switchShaders(isSkybox){
 //    gl.uniform1f(gl.getUniformLocation(shaderProgram, "uIsSkybox"), isSkybox);
 //}
 /**
-* Function to setup the cubemap texture for the skybox and teapot.
+* Function to setup the cubemap texture for the skybox a.
 * @return None
 */
-function setupCubeMap() {
-    // TODO: Initialize the Cube Map, and set its parameters
-    // See usage of gl.createTexture
+
+
+function resetMesh() {
+    initMesh();
+    initBuffers(false);
+}
+
+
+/*
+ * Main rendering code 
+ */
+
+// Basic rendering parameters
+var mvMatrix = mat4.create();                   // Model-view matrix for the main object
+var pMatrix = mat4.create();                    // Projection matrix
+
+// Lighting control
+var lightMatrix = mat4.create();                // Model-view matrix for the point light source
+var lightPos = vec3.create();                   // Camera-space position of the light source
+
+// Animation related variables
+var rotY = 0.0;                                 // object rotation
+var rotY_light = 0.0;                           // light position rotation
+
+var transVec = vec3.create();
+
+function setUniforms() {
+    gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
+    gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
+
+    var nMatrix = mat4.transpose(mat4.inverse(mvMatrix));
+    gl.uniformMatrix4fv(shaderProgram.nMatrixUniform, false, nMatrix);
+
+    gl.uniform3fv(shaderProgram.lightPosUniform, lightPos);
+}
+
+var drawMode;
+function drawScene() {
+    gl.useProgram(shaderProgram);
+    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    mat4.perspective(50, gl.viewportWidth/gl.viewportHeight, 0.1, 100.0, pMatrix);
+
+    mat4.identity(lightMatrix);
+
+    //light animation
     
+    mat4.rotateY(lightMatrix, rotY);
+    
+    
+    mat4.translate(lightMatrix, [0.0, 0.5, -10.0]);
+    //mat4.rotateX(lightMatrix, 0.3);
+    mat4.rotateY(lightMatrix, rotY_light);
+    
+    lightPos.set([0.0, 2.5, 3.0]);
+    mat4.multiplyVec3(lightMatrix, lightPos);
+    
+    mat4.identity(mvMatrix);
+    
+    mvPushMatrix();
+    mat4.translate(mvMatrix, [0.0, -0.1, -2.0]);
+    //mat4.rotateX(mvMatrix, 0.3);
+    
+    mat4.rotateX(mvMatrix, 0.2);
+    
+    //cam animation
+    
+    mat4.rotateY(mvMatrix, rotY);
+    mat4.translate(mvMatrix, transVec);
+    
+    setUniforms();
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexNormalBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, 3, gl.FLOAT, false, 0, 0);
+
+    // Normal mode
+    
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);        
+    gl.drawElements(gl.TRIANGLES, clothIndex.length, gl.UNSIGNED_SHORT, 0);
+    mvPopMatrix();
+
+}
+
+/**************skybox code*********/
+
+function getSkyboxShader(gl, id) {
+      var shaderScript = document.getElementById(id);
+      if (!shaderScript) {
+          return null;
+      }
+
+      var str = "";
+      var k = shaderScript.firstChild;
+      while (k) {
+          if (k.nodeType == 3)
+              str += k.textContent;
+          k = k.nextSibling;
+      }
+
+      var shader;
+      if (shaderScript.type == "x-shader/x-fragment") {
+          shader = gl.createShader(gl.FRAGMENT_SHADER);
+      } else if (shaderScript.type == "x-shader/x-vertex") {
+          shader = gl.createShader(gl.VERTEX_SHADER);
+      } else {
+          return null;
+      }
+
+      gl.shaderSource(shader, str);
+      gl.compileShader(shader);
+
+      if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+          alert(gl.getShaderInfoLog(shader));
+          return null;
+      }
+
+      return shader;
+  }
+
+var skyboxShaderProgram;
+  function initSkyboxShaders() {
+    var skyboxFragmentShader = getSkyboxShader(gl, "skybox-shader-fs");
+    var skyboxVertexShader = getSkyboxShader(gl, "skybox-shader-vs");
+
+    skyboxShaderProgram = gl.createProgram();
+    gl.attachShader(skyboxShaderProgram, skyboxVertexShader);
+    gl.attachShader(skyboxShaderProgram, skyboxFragmentShader);
+    gl.linkProgram(skyboxShaderProgram);
+
+    if (!gl.getProgramParameter(skyboxShaderProgram, gl.LINK_STATUS)) {
+      alert("Could not initialise shaders");
+    }
+
+    gl.useProgram(skyboxShaderProgram);
+
+    skyboxShaderProgram.vertexPositionAttribute = gl.getAttribLocation(skyboxShaderProgram, "aVertexPosition");
+    gl.enableVertexAttribArray(skyboxShaderProgram.vertexPositionAttribute);
+
+    skyboxShaderProgram.pMatrixUniform = gl.getUniformLocation(skyboxShaderProgram, "uPMatrix");
+    skyboxShaderProgram.mvMatrixUniform = gl.getUniformLocation(skyboxShaderProgram, "uMVMatrix");
+    skyboxShaderProgram.samplerUniform = gl.getUniformLocation(skyboxShaderProgram, "uSampler");
+  }
+
+function setMatrixUniforms() {
+    gl.uniformMatrix4fv(skyboxShaderProgram.pMatrixUniform, false, pMatrix);
+    gl.uniformMatrix4fv(skyboxShaderProgram.mvMatrixUniform, false, mvMatrix);
+ }
+
+ function degToRad(degrees) {
+        return degrees * Math.PI / 180;
+    }
+
+var cubeVertexPositionBuffer;
+var cubeVertexIndexBuffer;
+
+function initSkyboxBuffers() {
+    cubeVertexPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER,cubeVertexPositionBuffer);
+    var vertices = [
+    // Front face
+    -10.0, -10.0,  10.0,
+     10.0, -10.0,  10.0,
+     10.0,  10.0,  10.0,
+    -10.0,  10.0, 10.0,
+
+    // Back face
+    -10.0, -10.0, -10.0,
+    -10.0,  10.0, -10.0,
+     10.0,  10.0, -10.0,
+     10.0, -10.0, -10.0,
+
+    // Top face
+    -10.0,  10.0, -10.0,
+    -10.0,  10.0,  10.0,
+     10.0,  10.0,  10.0,
+     10.0,  10.0, -10.0,
+    // Bottom face
+    -10.0, -10.0, -10.0,
+     10.0, -10.0, -10.0,
+     10.0, -10.0,  10.0,
+    -10.0, -10.0,  10.0,
+
+    // Right face
+     10.0, -10.0, -10.0,
+     10.0,  10.0, -10.0,
+     10.0,  10.0,  10.0,
+     10.0, -10.0,  10.0,
+
+    // Left face
+    -10.0, -10.0, -10.0,
+    -10.0, -10.0,  10.0,
+    -10.0,  10.0,  10.0,
+    -10.0,  10.0, -10.0
+  ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    cubeVertexPositionBuffer.itemSize = 3;
+    cubeVertexPositionBuffer.numItems = 24;
+
+    cubeVertexIndexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
+    var cubeVertexIndices = [
+      0, 1, 2,      0, 2, 3,    // Front face
+      4, 5, 6,      4, 6, 7,    // Back face
+      8, 9, 10,     8, 10, 11,  // Top face
+      12, 13, 14,   12, 14, 15, // Bottom face
+      16, 17, 18,   16, 18, 19, // Right face
+      20, 21, 22,   20, 22, 23  // Left face
+    ]
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeVertexIndices),gl.STATIC_DRAW);
+    cubeVertexIndexBuffer.itemSize = 1;
+    cubeVertexIndexBuffer.numItems = 36;
+}
+
+function initSkyboxCubeMap(){
     cubeTexture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_CUBE_MAP, cubeTexture);
 
-
-    
     var cubeImage = new Image();
     cubeImage.onload = function() {
         gl.bindTexture(gl.TEXTURE_CUBE_MAP, cubeTexture);
@@ -162,98 +369,35 @@ function setupCubeMap() {
         
     }
     cubeImage5.src = "skybox/neg-z.JPG";
-    
+    //gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_R, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.LINEAR); 
 }
 
-function resetMesh() {
-    initMesh();
-    initBuffers(false);
-}
-
-
-/*
- * Main rendering code 
- */
-
-// Basic rendering parameters
-var mvMatrix = mat4.create();                   // Model-view matrix for the main object
-var pMatrix = mat4.create();                    // Projection matrix
-
-// Lighting control
-var lightMatrix = mat4.create();                // Model-view matrix for the point light source
-var lightPos = vec3.create();                   // Camera-space position of the light source
-
-// Animation related variables
-var rotY = 0.0;                                 // object rotation
-var rotY_light = 0.0;                           // light position rotation
-
-var transVec = vec3.create();
-
-function setUniforms() {
-    gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
-    gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
-
-    var nMatrix = mat4.transpose(mat4.inverse(mvMatrix));
-    gl.uniformMatrix4fv(shaderProgram.nMatrixUniform, false, nMatrix);
-
-    gl.uniform3fv(shaderProgram.lightPosUniform, lightPos);
-}
-
-var drawMode;
-function drawScene() {
+function drawSkybox() {
+    gl.useProgram(skyboxShaderProgram);
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    //gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    mat4.perspective(50, gl.viewportWidth/gl.viewportHeight, 0.1, 100.0, pMatrix);
-
-    mat4.identity(lightMatrix);
-    
-    
-    
-    //light animation
-    
-    mat4.rotateY(lightMatrix, rotY);
-    
-    
-    mat4.translate(lightMatrix, [0.0, 0.5, -10.0]);
-    //mat4.rotateX(lightMatrix, 0.3);
-    mat4.rotateY(lightMatrix, rotY_light);
-    
-    lightPos.set([0.0, 2.5, 3.0]);
-    mat4.multiplyVec3(lightMatrix, lightPos);
-    
+    mat4.perspective(50, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
     mat4.identity(mvMatrix);
-    
-    mvPushMatrix();
-    mat4.translate(mvMatrix, [0.0, -0.1, -2.0]);
-    //mat4.rotateX(mvMatrix, 0.3);
-    
-    mat4.rotateX(mvMatrix, 0.2);
-    
-    //cam animation
-    
-    mat4.rotateY(mvMatrix, rotY);
-    mat4.translate(mvMatrix, transVec);
-    
-    setUniforms();
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
-    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+    //mat4.translate(mvMatrix, [2.0, 5.0, 0.0]);
+    //mat4.rotate(mvMatrix, degToRad(90), [0, 1, 0]);
+    gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
+    gl.vertexAttribPointer(skyboxShaderProgram.vertexPositionAttribute, cubeVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexNormalBuffer);
-    gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, 3, gl.FLOAT, false, 0, 0);
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_CUBE_MAP, cubeTexture);
+    gl.uniform1i(skyboxShaderProgram.samplerUniform, 0);
 
-    // Normal mode
-    
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);        
-    gl.drawElements(gl.TRIANGLES, clothIndex.length, gl.UNSIGNED_SHORT, 0);
-    mvPopMatrix();
-
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
+    setMatrixUniforms();
+    gl.drawElements(gl.TRIANGLES, cubeVertexIndexBuffer.numItems,gl.UNSIGNED_SHORT,0);
 }
+/*****************endskybox code*************/
 
 
 var lastTime = 0;
@@ -332,16 +476,9 @@ function tick() {
         handleKey();
       
     }
-    lastTime = timeNow;     
-    
-    initShaders();
-    drawScene();
-    
-    initShaders2();
-    setupSkybox();
+    lastTime = timeNow;  
+    drawScene();   
     drawSkybox();
-
-    
 
     if ( animated ) {
         var timeStep = 0.001;
@@ -365,14 +502,21 @@ function webGLStart() {
 
     initGL(canvas);
 
+
     initMesh();
+    initShaders();
     initBuffers(true);
-    setupSkybox();
+
+    initSkyboxShaders();
+    initSkyboxBuffers();
+    initSkyboxCubeMap();
+    
+
+    
     gl.clearColor(0.3, 0.3, 0.3, 1.0);
     gl.enable(gl.DEPTH_TEST);
     gl.disable(gl.CULL_FACE);
     drawMode = 0;
-    
-    setupCubeMap();
+
     tick();
 }
